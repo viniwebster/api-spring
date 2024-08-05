3a77730a923337aa82a41e8e3ae18ed079fd8381
viniwebster <viniciuswebster99@gmail.com>
2024-08-05 15:31:28 -0300

    Implements entity Order, OrderRepository, OrderService and using association many to one and one to many

--- src/main/java/com/viniwebs/entities/User.java
+++ src/main/java/com/viniwebs/entities/User.java
@@ -1,9 +1,12 @@
 package com.viniwebs.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 @Entity
 @Table(name = "tb_user")
@@ -18,6 +21,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private final List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -69,6 +76,9 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
---
+++ src/main/java/com/viniwebs/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.viniwebs.services;
+
+import com.viniwebs.entities.Order;
+import com.viniwebs.entities.User;
+import com.viniwebs.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Order findById(long id) {
+        Optional<Order> user = repository.findById(id);
+        return user.get();
+    }
+}
