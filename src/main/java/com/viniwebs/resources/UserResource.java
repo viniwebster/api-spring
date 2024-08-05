ba7c6661a4bd4cb5149a297cbd3026e7b70a679a
viniwebster <viniciuswebster99@gmail.com>
2024-08-05 13:29:34 -0300

    Implements UserService and create findAll and findById

--- src/main/java/com/viniwebs/resources/UserResource.java
+++ src/main/java/com/viniwebs/resources/UserResource.java
@@ -1,18 +1,28 @@
 package com.viniwebs.resources;
 
 import com.viniwebs.entities.User;
+import com.viniwebs.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
 
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Vinicius", "vinicius@gmail.com", "99999999", "1234");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/viniwebs/services/UserService.java
@@ -0,0 +1,26 @@
+package com.viniwebs.services;
+
+
+import com.viniwebs.entities.User;
+import com.viniwebs.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(long id) {
+        Optional<User> user = repository.findById(id);
+        return user.get();
+    }
+}
