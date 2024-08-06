1c23a89cb4ee3d6d1306f2b3172700e642719109
viniwebster <viniciuswebster99@gmail.com>
2024-08-06 16:18:59 -0300

    implements method post from user

--- src/main/java/com/viniwebs/resources/UserResource.java
+++ src/main/java/com/viniwebs/resources/UserResource.java
@@ -5,7 +5,9 @@ import com.viniwebs.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,5 +26,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+        User obj = service.save(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
